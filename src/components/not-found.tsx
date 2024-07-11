b5047ac0e39b5e7679830056d5e86597d045fbbc
YakovDronov <77423384+YakovDronov@users.noreply.github.com>
2024-07-11 20:42:29 +0700

    Added routing to the page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -4,14 +4,14 @@ function NotFound(): JSX.Element {
   return (
     <div className="center-container">
       <div className="error-container">
-        <h1 className="error-title">Ошибка 404 – Страница не найдена</h1>
+        <h1 className="error-title">Error 404 - Page not found</h1>
         <p className="error-message">
-          Кажется, вы потерялись! Запрашиваемая страница не найдена.
+          I think you're lost! The requested page was not found.
         </p>
         <p className="error-message">
-          Вернитесь на
+          Go back to the
           <Link to="/" className="return-link">
-            <span>главную страницу</span>
+            <span>home page</span>
           </Link>
         </p>
       </div>
